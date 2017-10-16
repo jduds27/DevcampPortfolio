35b4520df686170660b86a6111bc65dc08cae61a
Jason Dudley <Jduds27@jasons-mbp.ad.uvu.edu>
2017-10-16 11:36:17 -0600

    added edit and new portfolio items

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,10 +1,26 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio, only: [:show, :edit, :update, :destroy]
   def index
     @portfolio_items = Portfolio.all
   end
 
   def new
     @portfolio_item = Portfolio.new
+  end
+
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+
+    def update
+     @portfolio_item = Portfolio.find(params[:id])
+     respond_to do |format|
+      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+        format.html { redirect_to portfolios_path, notice: 'Portfolio was successfully updated.' }
+      else
+        format.html { render :edit }
+      end
+    end
   end
 
   def create
@@ -18,5 +34,11 @@ class PortfoliosController < ApplicationController
       end
     end
   end
+
+   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_portfolio
+      @portfolio_item = Portfolio.find(params[:id])
+    end
 
 end
