b4ae097cfba94e4eb3dbc527bb931e3fe2aa155a
Jason Dudley <jason.dudley27@gmail.com>
2017-10-25 09:59:35 -0600

    added petergate for auth

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -68,8 +68,7 @@ class BlogsController < ApplicationController
       @blog.draft!
     end
     redirect_to blogs_url, notice: 'Post status has been updated.'
-  end
-
+    
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_blog
