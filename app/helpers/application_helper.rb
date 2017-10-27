b6de5dac2b83446aeb840e52da952842dc62d835
Jason Dudley <jason.dudley27@gmail.com>
2017-10-27 11:27:32 -0600

    updated sidebar added fav-cons updated footer and added kaminari

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,6 +17,6 @@ module ApplicationHelper
   end
 
   def copyright_generator
-    JdudsViewTool::Renderer.copyright 'Jason Dudley', 'All rights reserved'
+    JdudsViewTool::Renderer.copyright 'Jason Dudley ','All rights reserved'
   end
 end
