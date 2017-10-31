fd8d8dd5df0553246b2496cf5108bcbec9c9f08e
Jason Dudley <jason.dudley27@gmail.com>
2017-10-31 09:30:20 -0600

    implemented persistant position for drag and drop interface for portfolio page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register'}
-  resources :portfolios, except: [:show]
+  resources :portfolios, except: [:show] do
+    put :sort, on: :collection
+  end
   get 'angular-items', to: 'portfolios#angular'
   get 'portfolio/:id', to: 'portfolios#show', as: 'portfolio_show'
 
@@ -14,6 +16,4 @@ Rails.application.routes.draw do
   end
 
   root to: 'pages#home'
-
 end
-
